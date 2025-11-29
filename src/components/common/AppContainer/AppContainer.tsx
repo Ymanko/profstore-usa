ad08f241f730fde04ef861e172b75bf493f6a983
Alex <88508533+Saniokkk@users.noreply.github.com>
2025-11-29 03:10:14 +0200

    add header

---
+++ src/components/common/AppContainer/AppContainer.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+import s from './styles.module.scss';
+
+type TAppContainerProps = {
+  children: ReactNode;
+  classes?: string;
+};
+
+export const AppContainer = ({
+  children,
+  classes = ''
+}: TAppContainerProps) => {
+  return <div className={`${s.container} ${classes}`}>{children}</div>;
+};
