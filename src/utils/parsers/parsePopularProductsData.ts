41450e6c2d92d27c0f3c87726dac845d9a7b3878
josserden <josserden@gmail.com>
2025-12-20 19:37:30 +0200

    feat: improve popular products image parsing with MediaImage type guard

--- src/utils/parsers/parsePopularProductsData.ts
+++ src/utils/parsers/parsePopularProductsData.ts
@@ -1,14 +1,21 @@
 import { getLastSegment } from './getLastSegment';
 
-import type { ProductEdge } from '@/lib/graphql/graphql';
+import type { MediaImage, ProductEdge } from '@/lib/graphql/graphql';
+
+function isMediaImage(ref: any): ref is MediaImage {
+  return ref?.__typename === 'MediaImage' || ref?.image !== undefined;
+}
 
 export function parsePopularProductsData(products: ProductEdge[]) {
   return products.map(product => {
     const node = product.node;
+    const imageMetafield = node.metafields?.find(m => m?.key === 'custom_product_image');
+    const imageRef = imageMetafield?.reference;
+
     return {
       productId: getLastSegment(node.id),
       title: node.title,
-      image: node.metafields?.find(m => m?.key === 'custom_product_image')?.reference?.image?.url ?? null,
+      image: (imageRef && isMediaImage(imageRef) ? imageRef.image?.url : null) ?? null,
       price: node.priceRange.minVariantPrice.amount,
       oldPrice: node.compareAtPriceRange?.minVariantPrice.amount ?? null,
       description: node.metafields?.find(m => m?.key === 'short_description')?.value ?? null,
