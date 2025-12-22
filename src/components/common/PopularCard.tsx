e01d5e80c7941d6e3978e95b4b4f314aff42606b
Developer <karlsonzp2000@gmail.com>
2025-12-22 17:08:08 +0200

    fix: add relative positioning to PopularCard component for layout adjustment

--- src/components/common/PopularCard.tsx
+++ src/components/common/PopularCard.tsx
@@ -128,7 +128,7 @@ export function PopularProductBanner({ product }: PopularProductBannerProps) {
           />
         </div>
 
-        <div className='col-span-2 ml-5 flex flex-col items-baseline gap-7'>
+        <div className='col-span-2 ml-5 flex flex-col items-baseline gap-7 relative'>
           {/*Price*/}
           <div className='flex items-baseline gap-3'>
             {discount > 0 ? (
