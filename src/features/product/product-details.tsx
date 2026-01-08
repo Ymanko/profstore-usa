58b029fcf07608d89858bb30f24597e9edc7ecaa
josserden <josserden@gmail.com>
2026-01-08 12:36:22 +0200

    feat: implement product details component with error handling and improved layout

--- src/features/product/product-details.tsx
+++ src/features/product/product-details.tsx
@@ -1,9 +1,26 @@
 'use client';
 
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+import { NotFound } from '@/features/layout/not-found';
+import { Show } from '@/shared/components/common/show';
+import { Typography } from '@/shared/components/ui/typography';
+import { getProductQueryOptions } from '@/shared/queries/products/get-product';
+
 export function ProductDetails({ handle }: { handle: string }) {
+  const { data: product } = useSuspenseQuery(getProductQueryOptions(handle));
+
+  const images = product?.images.edges.map(edge => edge.node) || [];
+
+  console.log('Product Details:', product);
+
   return (
     <div className='container'>
-      <h1 className='text-3xl font-bold'>{handle}</h1>
+      <Show when={product} fallback={<NotFound>Product not found</NotFound>}>
+        <Typography variant='h1' as='h1'>
+          {product?.title}
+        </Typography>
+      </Show>
     </div>
   );
 }
