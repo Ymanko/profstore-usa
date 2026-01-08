251a431234c7c9b2617ee06ea3c6f0ab37bee82c
josserden <josserden@gmail.com>
2026-01-08 12:17:44 +0200

    feat: enhance product page by ensuring query data for product details

--- src/app/[category]/[subcategory]/[product]/page.tsx
+++ src/app/[category]/[subcategory]/[product]/page.tsx
@@ -1,6 +1,7 @@
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
 import { ProductDetails } from '@/features/product/product-details';
+import { getProductQueryOptions } from '@/shared/queries/products/get-product';
 
 interface ProductPageProps {
   params: Promise<{
@@ -11,6 +12,8 @@ interface ProductPageProps {
 export default async function ProductPage({ params }: ProductPageProps) {
   const { product } = await params;
   const queryClient = new QueryClient();
+
+  await queryClient.ensureQueryData(getProductQueryOptions(product));
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
