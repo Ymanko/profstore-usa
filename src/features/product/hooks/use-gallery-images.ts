0816fe9800434201357cf0cc6b18c3f495932fbe
josserden <josserden@gmail.com>
2026-01-09 12:56:27 +0200

    feat: update thumbnail styles for improved layout and spacing in gallery component

--- src/features/product/hooks/use-gallery-images.ts
+++ src/features/product/hooks/use-gallery-images.ts
@@ -17,7 +17,7 @@ export function useGalleryImages(images: GalleryImage[]): ReactImageGalleryItem[
         thumbnail: image.url,
         originalClass: 'object-contain object-center w-full h-auto max-w-full',
         thumbnailClass:
-          'object-cover object-center w-auto h-auto aspect-[78/66] md:aspect-[120/102] xl:aspect-[162/136] mt-5 md:mt-0 mr-2.5 md:mr-0 md:mb-4.5',
+          'object-cover object-center w-auto h-auto aspect-[78/66] md:aspect-[120/102] xl:aspect-[162/136] mr-2.5 md:mb-4.5 md:mr-5',
         originalAlt: image?.altText || 'Product image',
       })),
     [images],
